5eaaf4c08934ae4ad5e8b6e272866dbd7efe3eb7
ysf <yuansssf@gmail.com>
2020-04-10 23:17:46 +0800

    自定义圆形图片

--- app/src/main/java/com/yuan/music_x/widget/CircleImageView.java
+++ app/src/main/java/com/yuan/music_x/widget/CircleImageView.java
@@ -2,6 +2,14 @@ package com.yuan.music_x.widget;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
@@ -13,15 +21,63 @@ import androidx.annotation.Nullable;
  **/
 @SuppressLint("AppCompatCustomView")
 public class CircleImageView extends ImageView {
+
+    private Paint mPaint;
+    private int mRadius;
+    private float mScale;
+    private Matrix matrix;
+
     public CircleImageView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public CircleImageView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
     }
 
     public CircleImageView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mPaint = new Paint();
+        matrix = new Matrix();
+    }
+
+    //注意做好图片的缩放
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int size = Math.min(getMeasuredWidth(), getMeasuredHeight());
+        mRadius = size / 2;
+        setMeasuredDimension(size, size);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Bitmap bitmap = drawableToBitmap(getDrawable());
+
+        BitmapShader bitmapShader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        //计算缩放比例
+        mScale = (mRadius * 2.0f) / Math.min(bitmap.getHeight(), bitmap.getWidth());
+
+        matrix.setScale(mScale, mScale);
+        bitmapShader.setLocalMatrix(matrix);
+
+        mPaint.setShader(bitmapShader);
+        canvas.drawCircle(mRadius, mRadius, mRadius, mPaint);   //中心坐标、半径、画笔
+    }
+
+    private Bitmap drawableToBitmap(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            return bitmapDrawable.getBitmap();
+        }
+        int w = drawable.getIntrinsicWidth();
+        int h = drawable.getIntrinsicHeight();
+        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, w, h);
+        drawable.draw(canvas);
+        return bitmap;
+
     }
 }
